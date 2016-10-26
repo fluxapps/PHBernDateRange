88a51e91a3aec68f5f0db2d35c074718b020f157
Theodor Truffer <tt@studer-raimann.ch>
2016-10-26 14:11:52 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'phbe_daterange';
 $version = '0.0.1';
-$ilias_min_version = '5.0.000';
+$ilias_min_version = '5.2.000';
 $ilias_max_version = '5.2.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
